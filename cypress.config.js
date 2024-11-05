8f21005db088a70ca893f64bafee10b76dfad176
fjuza1 <fjuza@ditec.sk>
2024-11-05 14:51:02 +0100

    SUPPORT_FILE

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('cypress');
-const { SOVATESTURL,TIMEOUT } = require('./cypress/support/config.js');
+const { SOVATESTURL,TIMEOUT, SUPPORT_FILE } = require('./cypress/support/config.js');
 const prostredia = require('./prostredia.js');
 
 module.exports = defineConfig({
   projectId: '3o13nv',
   e2e: {
+    supportFile: SUPPORT_FILE,
     baseUrl: SOVATESTURL,
     setupNodeEvents(on, config) {
       on('before:browser:launch', (browser = {}, launchOptions) => {
