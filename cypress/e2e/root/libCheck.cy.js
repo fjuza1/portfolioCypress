8d134fe5b4280ee6a20a315c88c41101a8882d8b
fjuza1 <fjuza@ditec.sk>
2024-11-05 15:35:44 +0100

    vfds

--- cypress/e2e/root/libCheck.cy.js
+++ cypress/e2e/root/libCheck.cy.js
@@ -8,5 +8,6 @@ describe('libs', () => {
         
         // Using the _.omit() method 
         cy.log(_.omit(obj, ['name', 'username']));
+        const formatedDate = moment().format('MMMM Do YYYY, h:mm:ss a');
     });
 });
